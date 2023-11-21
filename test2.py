e5ecbac846f9f02ba65c794fae9e8fb48bce9616
Xcatter <kismetkoranteng@yahoo.com>
2023-11-21 22:38:10 +0000

    msg

---
+++ test2.py
@@ -0,0 +1,117 @@
+# Twitter version 2 API to get account info endpoints
+import requests
+
+url = "https://twitter-v24.p.rapidapi.com/search/"
+querystring = {"query": "KismetKismet", "limit": "10"}
+twitter_headers = {
+    "X-RapidAPI-Key": "cc05abe945msh7cc26d5561cbae6p115cd3jsn85c96893f8fd",
+    "X-RapidAPI-Host": "twitter-v24.p.rapidapi.com"}
+response = requests.get(url, headers=twitter_headers, params=querystring)
+
+
+# Reformat the json response from Twitter to csv------------------------------------------------------------------
+# flat_data = {}
+def flatten(json_data, parent_key="", sep="."):
+    flat_data = {}
+    if isinstance(json_data, dict):
+        for key, value in json_data.items():
+            new_key = f"{parent_key}{sep}{key}" if parent_key else key
+            if new_key:  # Skip empty keys
+                if isinstance(value, (dict, list)):
+                    flat_data.update(flatten(value, new_key, sep=sep))
+                else:
+                    flat_data[new_key] = value
+    elif isinstance(json_data, list):
+        for index, value in enumerate(json_data):
+            new_key = f"{parent_key}{sep}{index}"
+            if new_key:  # Skip empty keys
+                if isinstance(value, (dict, list)):
+                    flat_data.update(flatten(value, new_key, sep=sep))
+                else:
+                    flat_data[new_key] = value
+    return flat_data
+
+
+def data_points(met):
+    url_data, follower, following, username, account_create_date = [], [], [], [], []
+    post_date_time, verified, geolocation, retweets = [], [], [], []
+    for i in range(11):
+        url_entry = met.get(
+            f"data.search_by_raw_query.search_timeline.timeline.instructions.0.entries.{i}.content"
+            f".itemContent.tweet_results.result.legacy.entities.urls.0.expanded_url")
+        if url_entry is None:
+            pass
+        else:
+            url_data.append(url_entry)
+
+        follower_entry = met.get(
+            f"data.search_by_raw_query.search_timeline.timeline.instructions.0.entries.{i}.content.itemContent."
+            f"tweet_results.result.core.user_results.result.legacy.followers_count")
+        if follower_entry is None:
+            pass
+        else:
+            follower.append(follower_entry)
+
+        following_entry = met.get(
+            f"data.search_by_raw_query.search_timeline.timeline.instructions.0.entries.{i}.content.itemContent."
+            f"tweet_results.result.core.user_results.result.legacy.friends_count")
+        if following_entry is None:
+            pass
+        else:
+            following.append(following_entry)
+
+        username_entry = met.get(
+            f"data.search_by_raw_query.search_timeline.timeline.instructions.0.entries.{i}.content.itemContent."
+            f"tweet_results.result.core.user_results.result.legacy.name")
+        if username_entry is None:
+            pass
+        else:
+            username.append(username_entry)
+
+        account_create_date_entry = met.get(
+            f"data.search_by_raw_query.search_timeline.timeline.instructions.0.entries.{i}.content.itemContent."
+            f"tweet_results.result.core.user_results.result.legacy.created_at")
+        if account_create_date_entry is None:
+            pass
+        else:
+            account_create_date.append(account_create_date_entry)
+
+        post_date_time_entry = met.get(
+            f"data.search_by_raw_query.search_timeline.timeline.instructions.0.entries.{i}.content.itemContent."
+            f"tweet_results.result.legacy.created_at")
+        if post_date_time_entry is None:
+            pass
+        else:
+            post_date_time.append(post_date_time_entry)
+
+        verified_entry = met.get(
+            f"data.search_by_raw_query.search_timeline.timeline.instructions.0.entries.{i}.content.itemContent."
+            f"tweet_results.result.core.user_results.result.verified")
+        if verified_entry is None:
+            pass
+        else:
+            verified.append(verified_entry)
+
+        geolocation_entry = met.get(
+            f"data.search_by_raw_query.search_timeline.timeline.instructions.0.entries.{i}.content.itemContent."
+            f"tweet_results.result.core.user_results.result.legacy.location")
+        if geolocation_entry is None:
+            pass
+        else:
+            geolocation.append(geolocation_entry)
+
+        retweets_entry = met.get(
+            f"data.search_by_raw_query.search_timeline.timeline.instructions.0.entries.{i}.content.itemContent."
+            f"tweet_results.result.legacy.retweet_count")
+        if retweets_entry is None:
+            pass
+        else:
+            retweets.append(retweets_entry)
+    print([url_data, follower, following, username, account_create_date, post_date_time, verified, geolocation,
+           retweets])
+    return [url_data, follower, following, username, account_create_date, post_date_time, verified, geolocation,
+            retweets]
+
+
+if __name__ == '__main__':
+    data_points(flatten(response.json()))
